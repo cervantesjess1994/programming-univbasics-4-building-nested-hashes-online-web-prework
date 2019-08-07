9ded7eac42da6fa759218bfde775a8375fca1c52
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-07 08:05:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,7 @@ end
 
 def monopoly_with_fourth_tier
 	{:railroads => {
-		:pieces => 4 {
-			:rent_in_dollars => {
+		:pieces => 4,:rent_in_dollars => {
 				:one_piece_owned => 25,
 				:two_pieces_owned => 50,
 				:three_pieces_owned => 100,
